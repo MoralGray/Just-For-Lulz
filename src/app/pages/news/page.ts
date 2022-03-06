acd9a49baa3ae2341b21e8bb0c34b72edc335a5b
MoralGray <68497585+MummersMoral@users.noreply.github.com>
2022-03-06 14:16:00 +0300

    JFL-0 news

--- src/app/pages/news/page.ts
+++ src/app/pages/news/page.ts
@@ -12,6 +12,39 @@ import {AfterViewInit, Component, OnDestroy, OnInit} from '@angular/core';
 })
 export class NewsPageComponent implements AfterViewInit, OnInit, AfterViewInit, OnDestroy {
 
+
+  filters = [
+    'Все',
+    'Новости',
+    'Статьи',
+    'Медиа'
+  ];
+
+  fields = [
+    {
+      id: '1',
+      title: 'Новости 1',
+      category: 'Новости',
+    },
+    {
+      id: '2',
+      title: 'Медиа 1',
+      category: 'Медиа',
+    },
+    {
+      id: '3',
+      title: 'Статьи 1',
+      category: 'Статьи',
+    }
+  ];
+  fieldsSotred = [
+    {
+      id: '',
+      title: '',
+      category: '',
+    }
+  ];
+
   constructor(
     // appService: AppService,
     // translateService: TranslateService,
@@ -26,11 +59,24 @@ export class NewsPageComponent implements AfterViewInit, OnInit, AfterViewInit, 
   }
 
   ngOnInit() {
-
+    this.fieldsSotred = this.fields;
   }
 
   ngOnDestroy() {
 
+  }
+
+  filterSort(filter: string) {
+
+    if (filter === 'Все') {
+      return this.fieldsSotred = this.fields;
+    }
+
+    this.fieldsSotred = this.fields.filter((field: any) => {
+      return field.category === filter;
+    });
+
+    return null;
   }
 
 }
